51577b27f67c4ea7cc7261bb507a8ff5a058c693
Ishan Thilina Somasiri <ishan@ishans.info>
2012-08-28 22:16:27 +0530

    Now copies the autostart files to the proper palce

--- install.sh
+++ install.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+echo "Begining installation"
+
 INSTALLATION_DIR=".setBrightness"
+CURRENT_DIR=$PWD
+START_APP_DIR=$HOME"/.config/autostart"
 
 #move to home
-#cd ~
+cd ~
 
 #Check for the existence of the installation folder
-if [ ! -d "~/$INSTALLATION_DIR" ]; then
+if [ ! -d "$INSTALLATION_DIR" ]; then
     #create the target folder
     mkdir $INSTALLATION_DIR
-    
-    
+    #copy the source file
+    cp $CURRENT_DIR"/src/setBrightness.py" $INSTALLATION_DIR
+
+    #check for the existence of the startup app folder
+    if [ -d "$START_APP_DIR" ]; then
+	#copy the auto start file
+	cp $CURRENT_DIR"/conf/setBrightness.desktop" $START_APP_DIR
+	
+    else
+	#create the directory
+	mkdir $START_APP_DIR
+	#copy the auto start file
+	cp $CURRENT_DIR"/conf/setBrightness.desktop" $START_APP_DIR
+
+    fi
+    echo "Installation finished successfully!"
+        
 else
     echo "The target directory for installation," $INSTALLATION_DIR ",already exists in your home folder"
     echo "Exiting installation."
